8ec496075832dcaf274669362847e73e0fdf50a2
Kenneth Calamay <dev+kenneth@proudcloud.net>
2012-11-16 20:07:16 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewBarcode::Application.config.secret_token = 'b44134adeac64196d8e4d7a3e08303c0a9bee05d67f2a23bb861525199aeff422e1a847308b055c09919a56d2be5b83a53afc1b6f8cd3978b7d2fc61c3b18bae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NewBarcode::Application.config.session_store :cookie_store, key: '_new_barcode_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NewBarcode::Application.config.session_store :active_record_store
