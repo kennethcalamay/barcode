0389c268514edc897e1df7e94f79a9685777ddfb
Kenneth <github@kennethcalamay.com>
2012-11-26 22:34:08 +0800

    Sort registrants

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,9 @@
 class RegistrationsController < ApplicationController
   def show
     date = Date.parse(params[:id])
-    @registrations = Registration.where('created_at > ? and created_at < ?',
+    registrations = Registration.where('created_at > ? and created_at < ?',
                                         date.beginning_of_day, date.end_of_day).includes(:student)
+    @students = registrations.map{|r| r.student}
+    @students = @students.reject{|s| s.last_name.nil? }.sort_by!{ |s| s[:last_name]}
   end
 end
